423066673939c10e172f963f90d67909bde6039d
Srijan Srivastava <srijan.srivastav2@gmail.com>
2024-05-19 18:47:10 +0800

    Initial commit

---
+++ nvim/lua/main/plugins/bufferline.lua
@@ -0,0 +1,10 @@
+return{
+  'akinsho/bufferline.nvim', 
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  opts = {
+    options = {
+      mode = 'tabs',
+      seperator_style = 'slant'
+    }
+  }
+}
